7196a87a91e46b73e0a9b6b10ea65ce78637acb4
t-eldar <teldar6077@gmail.com>
2024-04-17 18:34:41 +0300

    feat: add text input component

---
+++ src/components/ui/text-field/TextField.tsx
@@ -0,0 +1,25 @@
+import styles from "./TextField.module.css";
+
+import { ComponentProps, forwardRef } from "react";
+
+/** Text field props. */
+export type TextFieldProps = Omit<ComponentProps<"input">, "type"> & {
+  type: "text" | "password" | "email" | "number" | "tel" | "url";
+};
+
+/** Text field ref. */
+export type TextFieldRef = HTMLInputElement;
+
+/**
+ * Creates a text input field component with the specified props.
+ *
+ * @param {TextFieldProps} props - the properties for the text field
+ * @return {JSX.Element} the input element with the specified class and props
+ */
+export const TextField = forwardRef<TextFieldRef, TextFieldProps>(
+  (props, ref) => {
+    return <input ref={ref} className={styles.main} {...props} />;
+  }
+);
+
+export default TextField;
---
+++ src/components/ui/text-field/index.ts
@@ -0,0 +1,5 @@
+import TextField, { TextFieldProps, TextFieldRef } from "./TextField";
+
+export default TextField;
+export { TextField };
+export type { TextFieldProps, TextFieldRef };
