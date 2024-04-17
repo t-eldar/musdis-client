82f1f6b1754232f859ec92713502da2133abc253
t-eldar <teldar6077@gmail.com>
2024-04-17 18:35:59 +0300

    feat: init navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import Button from "@components/ui/button";
+
+type NavbarItem = {
+  icon: JSX.Element;
+} & (
+  | {
+      link: string;
+    }
+  | {
+      onClick: () => void;
+    }
+);
+
+type NavbarProps = {
+  items: NavbarItem[];
+};
+
+export const Navbar = ({ items }: NavbarProps) => {
+  return (
+    <div className={styles.container}>
+      {items.map((item, index) =>
+        "link" in item ? (
+          <NavLink key={index} className={styles.item} to={item.link}>
+            {item.icon}
+          </NavLink>
+        ) : (
+          <Button key={index} className={styles.item} onClick={item.onClick}>
+            {item.icon}
+          </Button>
+        )
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
