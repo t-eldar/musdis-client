f38bbbda7e61eca774c04c3a4e331a70fa340315
t-eldar <teldar6077@gmail.com>
2024-04-21 15:11:07 +0300

    feat: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
       "@public": `${path.resolve(__dirname, "./public/")}`,
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+
     },
   },
 });
