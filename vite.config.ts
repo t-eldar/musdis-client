fa0b972b4985cabba9c5d230b6a122c2601cc6bb
t-eldar <teldar6077@gmail.com>
2024-05-02 20:38:38 +0300

    feat: add folder aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import path from "path";
+import fs from "fs";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync("./cert/key.pem"),
+      cert: fs.readFileSync("./cert/cert.pem"),
+    },
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
@@ -13,9 +21,10 @@ export default defineConfig({
       "@public": `${path.resolve(__dirname, "./public/")}`,
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@app-types": path.resolve(__dirname, "./src/types"),
+      "@stores": path.resolve(__dirname, "./src/stores"),
       "@services": path.resolve(__dirname, "./src/services"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
-
     },
   },
 });
