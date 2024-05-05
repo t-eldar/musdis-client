7b2f1179e5a97fa6acb578b41239234b4f564243
t-eldar <teldar6077@gmail.com>
2024-05-05 13:37:42 +0300

    feat: add use fetch hook

---
+++ src/hooks/use-fetch/useFetch.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState, useCallback, DependencyList } from "react";
+import { useAwait } from "@hooks/use-await";
+
+export default function useFetch<
+  TFetch extends (abortSignal: AbortSignal) => Promise<unknown>
+>(
+  fetch: TFetch,
+  dependencies: DependencyList = []
+): {
+  data: Awaited<ReturnType<typeof fetch>> | undefined;
+  isLoading: boolean;
+  error: Error | undefined;
+  refetch: () => Promise<Awaited<ReturnType<TFetch>> | undefined>;
+} {
+  const [data, setData] = useState<Awaited<ReturnType<typeof fetch>>>();
+  const [isCleaned, setIsCleaned] = useState(false);
+  const { promise, isLoading, error } = useAwait<TFetch>(fetch, [
+    isCleaned,
+    ...dependencies,
+  ]);
+
+  const abortController = new AbortController();
+
+  const fetcher = useCallback(async (): Promise<
+    Awaited<ReturnType<TFetch>> | undefined
+  > => {
+    const result = await promise(
+      ...([abortController.signal] as Parameters<TFetch>)
+    );
+    setData(result);
+
+    return result;
+  }, [promise]); // eslint-disable-line
+
+  useEffect(() => {
+    fetcher();
+    return () => {
+      setIsCleaned(true);
+      abortController.abort();
+    };
+  }, [fetcher]); // eslint-disable-line
+
+  return {
+    data,
+    isLoading,
+    error,
+    refetch: fetcher,
+  };
+}
