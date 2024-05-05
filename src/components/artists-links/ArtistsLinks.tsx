cf4c2c68ae5ba9b9d16998581d1da422ce9853e2
t-eldar <teldar6077@gmail.com>
2024-05-05 13:33:50 +0300

    feat: add ArtistsLinks component

---
+++ src/components/artists-links/ArtistsLinks.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import styles from "./ArtistsLinks.module.css";
+import { combineClassNames } from "@utils/style-utils";
+
+type ArtistsLinksProps = {
+  className?: string;
+  artists: {
+    name: string;
+    slug: string;
+  }[];
+};
+
+const ArtistsLinks = ({ artists, className }: ArtistsLinksProps) => {
+  return (
+    <div className={combineClassNames(styles["container"], className)}>
+      {artists.map((artist, i) => (
+        <div key={artist.slug}>
+          {i !== 0 && ", "}
+          <Link to={`/artists/${artist.slug}`} className={styles["link"]}>
+            {artist.name}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ArtistsLinks;
---
+++ src/components/artists-links/index.ts
@@ -0,0 +1,4 @@
+import ArtistsLinks from "./ArtistsLinks";
+
+export { ArtistsLinks };
+export default ArtistsLinks;
