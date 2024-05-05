fdcdbc7189b5a7b2cc264dadf2fedfb631d9b082
t-eldar <teldar6077@gmail.com>
2024-05-05 13:36:56 +0300

    feat: add player overlay

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,11 @@
+import { AudioPlayerOverlay } from "@components/audio-player-overlay";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
       <Outlet />
+      <AudioPlayerOverlay />
     </>
   );
 };
