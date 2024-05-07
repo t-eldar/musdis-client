cc116d9f46a7b953a2087a40baffc25f465dbe9b
t-eldar <teldar6077@gmail.com>
2024-05-07 20:00:08 +0300

    refactor: change PagedDataResponse with zod schema

--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,25 +1,29 @@
+import { z } from "zod";
+
 /**
  * The response with wrapped data.
- * 
+ *
  * @template TData - Type of the response data.
  */
 export type DataResponse<TData> = {
   data: TData;
 };
 
+export const paginationInfoSchema = z.object({
+  currentPage: z.number(),
+  pageSize: z.number(),
+  totalCount: z.number(),
+  totalPages: z.number(),
+  hasPrevious: z.boolean(),
+  hasNext: z.boolean(),
+});
+
 /**
  * The response with wrapped collection of data and additional pagination info.
- * 
+ *
  * @template TData - Type of the response data.
  */
 export type PagedDataResponse<TData> = {
   data: TData[];
-  paginationInfo: {
-    currentPage: number;
-    pageSize: number;
-    totalCount: number;
-    totalPages: number;
-    hasPrevious: boolean;
-    hasNext: boolean;
-  };
+  paginationInfo: z.infer<typeof paginationInfoSchema>;
 };
