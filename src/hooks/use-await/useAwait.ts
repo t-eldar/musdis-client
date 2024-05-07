3803fc6776d0edeffb451e62807e472617c0ba81
t-eldar <teldar6077@gmail.com>
2024-05-07 20:04:10 +0300

    refactor: use error assertions from utils

--- src/hooks/use-await/useAwait.ts
+++ src/hooks/use-await/useAwait.ts
@@ -1,21 +1,11 @@
+import { isError } from "@utils/assertions";
 import { useState, useCallback, DependencyList } from "react";
-
-// eslint-disable-next-line
-function isError(value: any): value is Error {
-  return (
-    value &&
-    value.stack &&
-    value.message &&
-    typeof value.stack === "string" &&
-    typeof value.message === "string"
-  );
-}
 
 export function useAwait<
   // eslint-disable-next-line
   TFunction extends ((...args: any[]) => Promise<any>) | (() => Promise<any>)
 >(callback: TFunction, dependencies: DependencyList = []) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error>();
 
   const promise = useCallback<
@@ -33,8 +23,6 @@ export function useAwait<
         return await callback(...args);
       } catch (e) {
         if (isError(e)) {
-          console.log(e);
-
           setError(e);
         } else {
           setError(new Error("Some error occurred"));
