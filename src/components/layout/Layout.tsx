0a2a0d17a7c6244788d9a97585f2667809025933
t-eldar <teldar6077@gmail.com>
2024-05-07 20:09:53 +0300

    feat: add navbar to layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,30 @@
+import styles from "./Layout.module.css";
+
 import { AudioPlayerOverlay } from "@components/audio-player-overlay";
+import Navbar from "@components/navbar";
+import { NavbarItem } from "@components/navbar/Navbar";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+type LayoutProps = {
+  navbarItems: NavbarItem[];
+};
+
+const Layout = ({ navbarItems }: LayoutProps) => {
+  const [isOverlayActive, setIsOverlayActive] = useState(false);
   return (
-    <>
-      <Outlet />
-      <AudioPlayerOverlay />
-    </>
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <Navbar items={navbarItems} />
+      </div>
+
+      <div className={styles.content}>
+        <Outlet />
+        {isOverlayActive && <div className={styles["extra-footer"]} />}
+      </div>
+
+      <AudioPlayerOverlay onActiveChange={setIsOverlayActive} />
+    </div>
   );
 };
 
