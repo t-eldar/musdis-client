c4195ded30762830ff19c2142d40f088a2fef028
t-eldar <teldar6077@gmail.com>
2024-05-11 14:16:11 +0300

    feat: add new routes

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -8,9 +8,12 @@ import { ReleasesPage } from "@pages/releases-page";
 import { ArtistsPage } from "@pages/artists-page";
 import { ArtistPage } from "@pages/artist-page";
 import { NavbarItem } from "@components/navbar/Navbar";
-import { TbHome } from "react-icons/tb";
+import { TbHome, TbUser } from "react-icons/tb";
 import { SignInPage } from "@pages/sign-in-page";
 import SignUpPage from "@pages/sign-up-page";
+import { ProfilePage } from "@pages/profile-page";
+import withUser from "@hoc/with-user";
+import WithUser from "@components/wrappers/with-user";
 
 const App = () => {
   const navbarItems: NavbarItem[] = [
@@ -19,14 +22,18 @@ const App = () => {
       link: "/",
     },
     {
-      icon: <TbHome />,
-      link: "/",
+      icon: <TbUser />,
+      link: "/profile",
     },
   ];
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout navbarItems={navbarItems} />,
+      element: (
+        <WithUser>
+          <Layout navbarItems={navbarItems} />
+        </WithUser>
+      ),
       children: [
         { path: "/sandbox", element: <SandboxPage /> },
 
@@ -34,6 +41,7 @@ const App = () => {
 
         { path: "/sign-in", element: <SignInPage /> },
         { path: "/sign-up", element: <SignUpPage /> },
+        { path: "/profile", element: <ProfilePage /> },
 
         { path: "/releases", element: <ReleasesPage /> },
         { path: "/releases/:releaseSlug", element: <ReleasePage /> },
