fd5bf314ecdc87df7b14480ce58be5a4b6482297
t-eldar <teldar6077@gmail.com>
2024-05-16 21:42:46 +0300

    chore: test components

--- src/pages/sandbox/SandboxPage.tsx
+++ src/pages/sandbox/SandboxPage.tsx
@@ -1,9 +1,3 @@
-import CreateArtistForm from "@components/forms/artists/create-artist-form";
-import UpdateArtistForm from "@components/forms/artists/update-artist-form";
-import PageLoader from "@components/loaders/page-loader";
-import useFetch from "@hooks/use-fetch";
-import { getUserArtists } from "@services/artists";
-import { useAuthStore } from "@stores/auth-store";
 
 /**
  * Page for testing components in isolation.
@@ -18,32 +12,9 @@ const SandboxPage = () => {
     return arr;
   }
 
-  const user = useAuthStore((s) => s.user);
-  const { data: artists } = useFetch(
-    async (signal) => {
-      return await getUserArtists(user?.id || "", signal);
-    },
-    [user]
-  );
-
-  if (!artists) {
-    return <PageLoader />;
-  }
-
   return (
     <>
-      {/* <CreateReleaseForm artist={artists[0]} /> */}
-      {/* <CreateArtistForm /> */}
-      {artists && (
-        <UpdateArtistForm
-          artist={{ artistTypeSlug: artists[0].type.slug, ...artists[0] }}
-        />
-      )}
-
-      {/* <Alert variant="info">
-        <div>Hello</div>
-      </Alert> */}
-      {/* <AudioUploader onSubmit={() => {}} /> */}
+      <div>dsds</div>
     </>
   );
 };
