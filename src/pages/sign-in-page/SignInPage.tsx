302e01c3e0cb0195b531a6e906cd1d978425aece
t-eldar <teldar6077@gmail.com>
2024-05-19 19:15:52 +0300

    feat: improve sign up and sign in pages design

--- src/pages/sign-in-page/SignInPage.tsx
+++ src/pages/sign-in-page/SignInPage.tsx
@@ -1,5 +1,6 @@
 import styles from "./SignInPage.module.css";
 
+import { Footer } from "@components/footer";
 import { SignInForm } from "@components/forms/authentication/sign-in-form";
 import { Separator } from "@components/ui/separator";
 import { Link, useLocation, useNavigate } from "react-router-dom";
@@ -14,13 +15,23 @@ const SignInPage = () => {
 
   return (
     <div className={styles.container}>
-      <SignInForm className={styles.form} onSuccess={handleSuccess} />
+      <div className={styles["form-container"]}>
+        <div className={styles["image"]}>
+          <h1>Sign in</h1>
+        </div>
+        <div>
+          <SignInForm className={styles.form} onSuccess={handleSuccess} />
 
-      <span className={styles.text}>Don't have an account?</span>
-      <Link className={styles.link} to="/sign-up">
-        Sign Up!
-      </Link>
+          <div className={styles["link-container"]}>
+            <span className={styles.text}>Already have an account?</span>
+            <Link className={styles.link} to="/sign-up">
+              Sign up!
+            </Link>
+          </div>
+        </div>
+      </div>
       <Separator />
+      <Footer />
     </div>
   );
 };
--- src/pages/sign-up-page/SignUpPage.tsx
+++ src/pages/sign-up-page/SignUpPage.tsx
@@ -1,20 +1,31 @@
 import styles from "./SignUpPage.module.css";
 
-import { Separator } from "@components/ui/separator";
+import { Footer } from "@components/footer";
 import { SignUpForm } from "@components/forms/authentication/sign-up-form";
+import { Separator } from "@components/ui/separator";
 import { Link, useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <SignUpForm className={styles.form} onSuccess={() => navigate(-1)} />
+      <div className={styles["form-container"]}>
+        <div className={styles["image"]}>
+          <h1>Sign up</h1>
+        </div>
+        <div>
+          <SignUpForm className={styles.form} onSuccess={() => navigate(-1)} />
 
-      <span className={styles.text}>Already have an account?</span>
-      <Link className={styles.link} to="/sign-in">
-        Sign In!
-      </Link>
+          <div className={styles["link-container"]}>
+            <span className={styles.text}>Already have an account?</span>
+            <Link className={styles.link} to="/sign-in">
+              Sign In!
+            </Link>
+          </div>
+        </div>
+      </div>
       <Separator />
+      <Footer />
     </div>
   );
 };
